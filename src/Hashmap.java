48229dd532c1db81c4cbdd27b09cab2149fe313f
AnkitKumar111 <ankitwelcomesyou@gmail.com>
2017-04-30 10:41:50 -0700

    Assignment9.3
    An application having a Generic HashMap with Empcode as key and EmpName
    as value.

---
+++ src/Hashmap.java
@@ -0,0 +1,17 @@
+/*an application having a Generic HashMap
+  with Empcode as key and EmpName as value.*/
+import java.util.HashMap;
+import java.util.Map;
+public class Hashmap {
+	public static void main(String[] args) {		
+			  HashMap<Integer,String> HM=new HashMap<Integer,String>();  
+			  HM.put(1,"Ajay");  
+			  HM.put(2,"Sunil");  
+			  HM.put(3,"Sharma");  
+			  HM.put(4,"Rahul");  
+			  for(Map.Entry m:HM.entrySet()){  
+			   System.out.print(m.getKey()+" "); 
+			   System.out.println(m.getValue());  
+			  }  
+			 }
+	}
